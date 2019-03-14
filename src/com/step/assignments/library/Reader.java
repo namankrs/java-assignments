12a87da48cfaa17574a2f22cd9ed5ed4acae1103
naman sharma <44020825+namankrs@users.noreply.github.com>
2019-03-14 17:59:29 +0530

    Removed reduntant code in library

--- src/com/step/assignments/library/Reader.java
+++ src/com/step/assignments/library/Reader.java
@@ -26,28 +26,4 @@ public class Reader {
         this.books.remove(book);
         return true;
     }
-
-
-
-
-//    public ArrayList<Book> getBooks() {
-//        return books;
-//    }
-//
-//    public boolean addBook(Book book){
-//        if(books.contains(book)) return false;
-//        books.add(book);
-//        return true;
-//    }
-//
-//    public boolean returnBook(Book book){
-//        if(!books.contains(book)) return false;
-//        book.deAssign();
-//        books.remove(book);
-//        return true;
-//    }
-//
-//    public boolean checkBook(String name){
-//        return books.stream().anyMatch(book -> book.getName() == name);
-//    }
 }
